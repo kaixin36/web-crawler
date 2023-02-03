0f6ba2f851b313b25168ad45f9d8badadca25c7f
liufan <1014229769@qq.com>
2023-02-03 14:58:19 +0800

    添加考勤人员数-副本

---
+++ attendance/插入考勤记录.sql
@@ -0,0 +1,32 @@
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-27 00:00:00.000','2023-01-27 08:10:01.000','2023-01-27 08:10:01.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-27 00:00:00.000','2023-01-27 11:30:00.000','2023-01-27 11:30:00.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-27 00:00:00.000','2023-01-27 13:30:00.000','2023-01-27 13:30:00.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-27 00:00:00.000','2023-01-27 17:38:34.000','2023-01-27 17:38:34.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-28 00:00:00.000','2023-01-28 08:10:01.000','2023-01-28 08:10:01.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-28 00:00:00.000','2023-01-28 11:30:00.000','2023-01-28 11:30:00.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-28 00:00:00.000','2023-01-28 13:30:00.000','2023-01-28 13:30:00.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-28 00:00:00.000','2023-01-28 17:38:34.000','2023-01-28 17:38:34.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-29 00:00:00.000','2023-01-29 08:10:01.000','2023-01-29 08:10:01.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-29 00:00:00.000','2023-01-29 11:30:00.000','2023-01-29 11:30:00.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-29 00:00:00.000','2023-01-29 13:30:00.000','2023-01-29 13:30:00.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-29 00:00:00.000','2023-01-29 17:38:34.000','2023-01-29 17:38:34.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-30 00:00:00.000','2023-01-30 08:10:01.000','2023-01-30 08:10:01.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-30 00:00:00.000','2023-01-30 11:30:00.000','2023-01-30 11:30:00.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-30 00:00:00.000','2023-01-30 13:30:00.000','2023-01-30 13:30:00.000',1,null)
+Go
+INSERT INTO UserAttendanceRecord([UserNo],[AttendanceDate],[AttendanceTime],[CreateDate],[StatisticStatus],[Remark])VALUES('Z1231','2023-01-30 00:00:00.000','2023-01-30 17:38:34.000','2023-01-30 17:38:34.000',1,null)
+Go
